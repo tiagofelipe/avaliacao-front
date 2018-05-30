a2b6f528f200ac4eb14b88345ae940fe7f636e2a
Raphael Henrique <raphael.henrique@wtis.com.br>
2018-05-30 00:11:42 -0300

    Cadastro de estabelecimento (incompleto)

---
+++ src/app/estabelecimento/CadastroEstabelecimento.vue
@@ -0,0 +1,55 @@
+<script>
+import EstabelecimentoService from '../../domain/estabelecimento/services/estabelecimento'
+
+export default {
+  name: 'CadastroEstabelecimento',
+  data () {
+    return {
+      estabelecimento: {
+        cnpj: 'a',
+        nomeFantasia: 'b',
+        razaoSocial: 'c',
+        tipo: 'd'
+      }
+    }
+  },
+  mounted () {
+    this.$refs.cnpj.focus()
+  },
+  methods: {
+    cadastraEstabelecimento () {
+      EstabelecimentoService.cadastrar({...this.estabelecimento})
+        .then(result => {
+          console.log(result.data)
+        })
+        .catch(error => {
+          console.log(error.response.data)
+        })
+    }
+  }
+}
+</script>
+
+<template>
+  <q-page padding>
+    <form @submit.prevent="cadastraEstabelecimento">
+      <q-field icon="" class="q-my-md">
+        <q-input type="text" float-label="CNPJ" ref="cnpj" v-model="estabelecimento.cnpj" ></q-input>
+      </q-field>
+      <q-field icon="" class="q-my-md">
+        <q-input type="text" float-label="Nome Fantasia" v-model="estabelecimento.nomeFantasia" ></q-input>
+      </q-field>
+      <q-field icon="" class="q-my-md">
+        <q-input type="text" float-label="Razão Social" v-model="estabelecimento.razaoSocial" ></q-input>
+      </q-field>
+      <q-field icon="" class="q-my-md">
+        <q-input type="text" float-label="Tipo" v-model="estabelecimento.tipo" ></q-input>
+      </q-field>
+      <q-btn color="primary" type="submit" class="full-width">Cadastrar</q-btn>
+    </form>
+  </q-page>
+</template>
+
+<style scoped>
+
+</style>
