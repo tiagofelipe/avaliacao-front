e86db173d1f346736fb1b7f2dbaaf3d1a5fa97af
Raphael Henrique <raphael.henrique@wtis.com.br>
2018-06-03 20:52:20 -0300

    Auth layout style

--- src/app/auth/Main.vue
+++ src/app/auth/Main.vue
@@ -17,13 +17,34 @@ export default {
 </script>
 
 <template>
-  <q-page class="layout-padding">
-    <login></login>
+  <q-page class="layout-padding login-page">
+    <div class="login">
+      <span class="auth-title">Login</span>
+      <login></login>
+    </div>
   </q-page>
 </template>
 
 <style scoped>
   .full-height {
     height: 100vh !important;
+  }
+  .login-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url("assets/bg.png");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+  .login {
+    width: 450px;
+    background-color: #fff;
+    padding: 20px;
+  }
+  .auth-title {
+    font-size: 2rem;
+    font-weight: lighter;
   }
 </style>
