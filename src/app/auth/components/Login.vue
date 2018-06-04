497b662e5a266350aa1a6ab04f8a245a1157457c
Raphael Henrique <raphael.henrique@wtis.com.br>
2018-06-03 23:26:42 -0300

    loading, error message style and route redirect fix

--- src/app/auth/components/Login.vue
+++ src/app/auth/components/Login.vue
@@ -20,17 +20,19 @@ export default {
   },
   methods: {
     doLogin () {
-      console.log('teste')
+      this.$q.loading.show()
       this.$store.dispatch('attemptLogin', {...this.user})
         .then(() => {
-          if (this.$route.query.redirect) {
+          if (this.$route.query.redirect && this.$route.query.redirect !== '/logout') {
             this.$router.push({path: this.$route.query.redirect})
           } else {
             this.$router.push({ name: 'index' })
           }
+          this.$q.loading.hide()
         })
         .catch(error => {
-          this.error = error.erro
+          this.error = error.detail
+          this.$q.loading.hide()
         })
     }
   }
@@ -39,6 +41,7 @@ export default {
 
 <template>
   <form @submit.prevent="doLogin()">
+    <div class="error-msg">{{ error }}</div>
     <q-field icon="ion-person" class="q-my-md">
       <q-input type="text" float-label="Login" ref="username" v-model="user.username"></q-input>
     </q-field>
@@ -46,11 +49,15 @@ export default {
       <q-input type="password" float-label="Senha" v-model="user.password" ></q-input>
     </q-field>
     <q-btn color="primary" type="submit" class="full-width">Entrar</q-btn>
-    {{ error }}
   </form>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus">
   @import '~variables'
+  .error-msg
+    color: $negative;
+    font-size: small;
+    margin: 10px;
+    text-align: center;
 </style>
