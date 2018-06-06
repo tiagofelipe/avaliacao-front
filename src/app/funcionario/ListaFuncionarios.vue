f3b1452fe62eae70d7bcdaeb75d3e8baf25fff7e
Raphael Henrique <raphael.henrique@wtis.com.br>
2018-06-06 15:27:05 -0300

    Listing Funcionarios whit API data

--- src/app/funcionario/ListaFuncionarios.vue
+++ src/app/funcionario/ListaFuncionarios.vue
@@ -1,22 +1,33 @@
 <script>
+import FuncionarioService from '../../domain/funcionario/services/funcionario'
+
 export default {
   name: 'ListaFuncionarios',
   data () {
     return {
-      funcionarios: [
-        {
-          id: 1,
-          nome: 'Raphael Henrique Silva Oliva',
-          cargo: 'Developer',
-          nota: 5
-        },
-        {
-          id: 2,
-          nome: 'Sherlock Holmes',
-          cargo: 'Consulting detective',
-          nota: 4
-        }
-      ]
+      funcionarios: [],
+      isLoading: false,
+      errorMsg: null
+    }
+  },
+  mounted () {
+    this.getFuncionarios()
+  },
+  methods: {
+    getFuncionarios () {
+      this.isLoading = true
+      FuncionarioService.getFuncionarios(this.$store.getters.currentEstabelecimento.id)
+        .then(result => {
+          console.log(result.data)
+          this.funcionarios = result.data
+          this.isLoading = false
+        })
+        .catch(error => {
+          if (error.response.data.status === 404) {
+            this.errorMsg = 'Não há funcionários cadastrados'
+            this.isLoading = false
+          }
+        })
     }
   }
 }
@@ -24,24 +35,30 @@ export default {
 
 <template>
   <q-page padding>
-    <q-card inline class="card q-ma-sm" v-for="f in funcionarios" :key="f.id">
-      <q-card-media>
-        <img src="~assets/bg-menu.png" class="picture" height="150">
-      </q-card-media>
-      <q-card-title class="relative-position">
-        <div class="ellipsis"><small>{{ f.nome }}</small></div>
-        <q-rating slot="subtitle" v-model="f.nota" :max="5" ></q-rating>
-      </q-card-title>
-      <q-card-main>
-        <p>{{ f.cargo }}</p>
-        <!--p class="text-faded">Description</p-->
-      </q-card-main>
-      <q-card-separator ></q-card-separator>
-      <q-card-actions>
-        <q-btn flat round icon="edit" color="positive"></q-btn>
-        <q-btn flat round icon="delete" color="negative"></q-btn>
-      </q-card-actions>
-    </q-card>
+    <span v-if="errorMsg">{{ errorMsg }}</span>
+    <div v-else>
+      <q-card inline class="card q-ma-sm" v-for="f in funcionarios" :key="f.id">
+        <q-card-media>
+          <img src="~assets/bg-menu.png" class="picture" height="150">
+        </q-card-media>
+        <q-card-title class="relative-position">
+          <div class="ellipsis"><small>{{ f.nome }}</small></div>
+          <q-rating slot="subtitle" v-model="f.nota" :max="5" readonly></q-rating>
+        </q-card-title>
+        <q-card-main>
+          <p>{{ f.cargo }}</p>
+          <!--p class="text-faded">Description</p-->
+        </q-card-main>
+        <q-card-separator ></q-card-separator>
+        <q-card-actions align="around">
+          <q-btn flat color="positive">Editar</q-btn>
+          <q-btn flat color="negative">Excluir</q-btn>
+        </q-card-actions>
+      </q-card>
+    </div>
+    <q-inner-loading :visible="isLoading">
+      <q-spinner-gears size="50px" color="primary"></q-spinner-gears>
+    </q-inner-loading>
   </q-page>
 </template>
 
--- src/domain/funcionario/services/funcionario.js
+++ src/domain/funcionario/services/funcionario.js
@@ -11,5 +11,10 @@ export default {
     return http.post(config.baseUrl + '/api/funcionario/' + id + '/upload', file, { headers: { 'Content-Type': 'multipart/form-data' } })
       .then(result => Promise.resolve(result))
       .catch(error => Promise.reject(error))
+  },
+  getFuncionarios (id) {
+    return http.get(config.baseUrl + '/api/public/funcionario/estabelecimento/' + id)
+      .then(result => Promise.resolve(result))
+      .catch(error => Promise.reject(error))
   }
 }
