411b77e25495722c88d8f652cc10d36b93923320
Raphael Henrique <raphael.henrique@wtis.com.br>
2018-06-20 00:11:59 -0300

    adaptações novo layout (em progresso)

--- src/app/layouts/default.vue
+++ src/app/layouts/default.vue
@@ -6,7 +6,8 @@ export default {
   name: 'LayoutDefault',
   data () {
     return {
-      leftDrawerOpen: this.$q.platform.is.desktop
+      leftDrawerOpen: this.$q.platform.is.desktop,
+      opened: false
     }
   },
   computed: {
@@ -23,87 +24,29 @@ export default {
 </script>
 
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-layout-header  v-if="isLogged">
-      <q-toolbar color="primary" :inverted="$q.theme === 'ios'">
-        <q-btn flat dense round @click="leftDrawerOpen = !leftDrawerOpen" aria-label="Menu">
-          <q-icon name="menu" ></q-icon>
-        </q-btn>
-
-        <q-toolbar-title>
-          Software Avaliação
-          <div slot="subtitle">Bem-vindo!</div>
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-layout-header>
-
-    <q-layout-drawer v-model="leftDrawerOpen" :content-class="$q.theme === 'mat' ? 'bg-white' : null" v-if="isLogged">
-      <div class="menu-header">
-        <div class="user-avatar">
-          <span class="letter">{{ currentUser.nome.trim().substr(0, 1).toUpperCase() }}</span>
+  <div>
+    <header>
+      <div class="container-logo">
+        <div class="header-tools">
+          <div class="dropdown" :class="{ open: opened }">
+            <button class="bume-btn link btn-user-menu dropdown-toggle" @click="opened = !opened">{{ currentUser.email }}</button>
+            <ul class="bm-dropdown-menu">
+              <li class="bm-dropdown-item">Perfil</li>
+                <router-link to="/logout" tag="li" class="bm-dropdown-item">Sair</router-link>
+            </ul>
+          </div>
         </div>
-        <div class="user-name">{{ userName }}</div>
       </div>
-      <q-list no-border link inset-delimiter>
-        <q-item link to="/">
-          <q-item-side icon="ion-home" ></q-item-side>
-          <q-item-main>
-            <q-item-tile label>Dashboard</q-item-tile>
-          </q-item-main>
-        </q-item>
-        <q-item-separator></q-item-separator>
-        <q-collapsible icon="send" label="Estabelecimento">
-          <q-item link :to="{ name: 'estabelecimento.cadastro' }">
-            <q-item-side icon="add" ></q-item-side>
-            <q-item-main label="Cadastrar" ></q-item-main>
-          </q-item>
-        </q-collapsible>
-        <q-collapsible icon="send" label="Funcionário">
-          <q-item link :to="{ name: 'funcionario.cadastro' }">
-            <q-item-side icon="add" ></q-item-side>
-            <q-item-main label="Cadastrar" ></q-item-main>
-          </q-item>
-          <q-item link :to="{ name: 'funcionario.lista' }">
-            <q-item-side icon="list" ></q-item-side>
-            <q-item-main label="Ver todos" ></q-item-main>
-          </q-item>
-        </q-collapsible>
-        <q-item-separator></q-item-separator>
-        <q-item link to="/logout" v-if="isLogged">
-          <q-item-side icon="ion-log-out" ></q-item-side>
-          <q-item-main>
-            <q-item-tile label>Sair</q-item-tile>
-          </q-item-main>
-        </q-item>
-      </q-list>
-    </q-layout-drawer>
-
-    <q-page-container>
-      <router-view ></router-view>
-    </q-page-container>
-  </q-layout>
+    </header>
+    <div class="container fill">
+      <div class="row fill">
+        <div class="col-xs-12 custom-vh">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="stylus">
-  @import '~variables';
-  .menu-header
-    background-color: $secondary;
-    background-image: url('../../assets/bg-menu.png')
-    height: 150px;
-    display: flex;
-    align-items: center;
-    color: #fff;
-  .user-avatar
-    background-color: $primary;
-    font-size: 1.5rem;
-    margin: 10px 20px 10px 10px;
-    padding: 25px;
-    -webkit-border-radius: 50%;
-    -moz-border-radius: 50%;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  .letter
-    position: absolute;
+<style scoped>
 </style>
--- src/app/pages/index.vue
+++ src/app/pages/index.vue
@@ -29,9 +29,9 @@ export default {
       this.isLoading = true
       EstabelecimentoService.getEstabelecimentosByUser(this.currentUser.id)
         .then(result => {
-          if (result.data.estabelecimentos.length === 1) {
-            this.selecionaEstabelecimento(result.data.estabelecimentos[0])
-          }
+          /* if (result.data.estabelecimentos.length === 1) {
+              this.selecionaEstabelecimento(result.data.estabelecimentos[0])
+            } */
           this.estabelecimentos = result.data.estabelecimentos
           this.isLoading = false
         }) // TODO: CATCH
@@ -44,14 +44,107 @@ export default {
 </script>
 
 <template>
-  <q-page padding>
-    <div v-if="!isCurrentEstabelecimentoVazio">
+  <div>
+    <div class="account-list-container" v-if="!isCurrentEstabelecimentoVazio">
       <estatisticas></estatisticas>
     </div>
     <div class="empty-msg" v-else-if="(estabelecimentos.length === 0) && !isLoading">
       <q-alert type="warning" message="Você não cadastrou estabelecimento"></q-alert>
     </div>
-    <div v-else-if="estabelecimentos.length > 1">
+    <div class="account-list-container" v-else-if="estabelecimentos.length > 0 && !isLoading">
+      <div class="row">
+        <div class="col-md-12">
+          <h3>Estabelecimentos Cadastrados</h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 opt-bar">
+          <div class="row">
+            <div class="col-xs-12 col-md-3">
+              <div class="collapse" id="accountSearch">
+                <div class="bm-ittybitty bm-ittybitty-left-icon">
+                  <input placeholder="Buscar" ng-model="vm.filters.byText" ng-model-options="{debounce: 250}" type="text" class="form-control bm-form-control input_m ng-pristine ng-untouched ng-valid ng-empty" aria-invalid="false">
+                  <span class="ittybitty-icon">
+                    <i class="fa fa-search"></i>
+                  </span>
+                </div>
+              </div>
+              <div class="bm-ittybitty bm-ittybitty-left-icon hidden-xs">
+                <input placeholder="Buscar" ng-model="vm.filters.byText" ng-model-options="{debounce: 250}" type="text" class="form-control bm-form-control input_m ng-pristine ng-untouched ng-valid ng-empty" aria-invalid="false">
+                <span class="ittybitty-icon">
+                  <i class="fa fa-search"></i>
+                </span>
+              </div>
+            </div>
+            <div class="col-xs-3">
+              <b>Status:</b>
+              <div class="btn-group">
+                <select class="bm-dropdown-select-simple ng-pristine ng-untouched ng-valid ng-not-empty" ng-model="vm.filters.selectedFilter" ng-init="vm.filters.selectedFilter = vm.filterOptions[0]" ng-options="filter as filter.name for filter in vm.filterOptions" aria-label="Estado da conta" ng-model-options="{trackBy: '$value.val'}" aria-invalid="false">
+                  <option label="Todas" value="object:13" selected="selected">Todas</option>
+                  <option label="Ativas" value="object:14">Ativas</option>
+                  <option label="Pausadas" value="object:15">Pausadas</option>
+                  <option label="Expiradas" value="object:16">Expiradas</option>
+                </select>
+              </div>
+            </div>
+            <div class="col-xs-5 col-sm-6 col-md-3">
+              <b>Expira em:</b>
+              <div class="btn-group">
+                <select class="bm-dropdown-select-simple ng-pristine ng-untouched ng-valid ng-not-empty" ng-model="vm.dateFilter" ng-options="filters for filters in vm.filterPaymentState" aria-invalid="false">
+                  <option label="Qualquer Data" value="string:Qualquer Data" selected="selected">Qualquer Data</option>
+                  <option label="Hoje" value="string:Hoje">Hoje</option>
+                  <option label="Amanhã" value="string:Amanhã">Amanhã</option>
+                  <option label="Próxima Semana" value="string:Próxima Semana">Próxima Semana</option>
+                  <option label="Próximo Mês" value="string:Próximo Mês">Próximo Mês</option>
+                  <option label="Mês Atual" value="string:Mês Atual">Mês Atual</option>
+                  <option label="Próximos 7 dias" value="string:Próximos 7 dias">Próximos 7 dias</option>
+                  <option label="Próximos 30 dias" value="string:Próximos 30 dias">Próximos 30 dias</option>
+                </select>
+              </div>
+            </div>
+            <div class="col-xs-4 col-sm-3">
+              <button ui-sref="wizard" class="bm-button bm-button-default bm-button-small pull-right" href="/a/wizard">Adicionar Conta</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="lista-contas">
+          <div class="conta ng-scope panel-active" v-for="e in estabelecimentos" :key="e.id">
+            <div class="conta-head">
+              <span class="conta-name ng-binding"><!--i class="fa fa-instagram"></i--> {{ e.nomeFantasia }}</span>
+            </div>
+            <div class="heading-conta">
+              <img profile-img="solutionplaceblindagem" class="img-circle profile-pic" src="https://api.bume.io/api/accounts/solutionplaceblindagem/picture">
+              <div class="seguidores">
+                <strong class="ng-binding">12.229</strong> <span>Seguidores</span>
+              </div>
+              <!-- Exibir este bloco abaixo apenas se houver novos seguidores -->
+              <div class="seguidores">
+                <strong class="ng-binding">
+                  <i ng-show="a.newFollowersSinceStart" class="fa fa-caret-up" aria-hidden="false">
+
+                  </i>2.364</strong> <span>Novos</span>
+              </div>
+            </div>
+            <div class="infos-conta">
+              <!-- Conta ativa sem erros -->
+              <div class="plan-content">
+                <div class="plan-data">
+                  <!--p ng-if="a.planTitle &amp;&amp; !a.remainingTime" class="ativa ng-scope">Conta Ativa</p>
+                  <p ng-if="a.planTitle &amp;&amp; !a.remainingTime" class="ng-binding ng-scope">(Plano Mensal)</p-->
+                </div>
+                <a class="">
+                  <button class="bm-button bm-button-small bm-button-secondary" @click="selecionaEstabelecimento(e)">Dashboard</button>
+                </a>
+              </div>
+            </div>
+            <!-- /infos-conta -->
+          </div>
+          <!-- end ngRepeat: a in vm.filteredAccounts = (vm.accounts | accountListFilter: vm.filters | accountListDateFilter: vm.dateFilter ) -->
+          <!-- conta -->
+        </div>
+      </div>
       <q-card inline class="q-ma-sm bigger" v-for="e in estabelecimentos" :key="e.id" style="max-width: 350px">
         <q-card-title>
           {{ e.nomeFantasia }}
@@ -60,7 +153,9 @@ export default {
         <q-card-main>
           main content
         </q-card-main>
-        <q-card-separator ></q-card-separator>
+        <q-card-separator >
+
+        </q-card-separator>
         <q-card-actions>
           <q-btn flat color="primary" @click="selecionaEstabelecimento(e)">Selecionar</q-btn>
         </q-card-actions>
@@ -69,8 +164,15 @@ export default {
     <q-inner-loading :visible="isLoading">
       <q-spinner-gears size="50px" color="primary"></q-spinner-gears>
     </q-inner-loading>
-  </q-page>
+  </div>
 </template>
 
-<style>
+<style scoped>
+  .conta {
+    width: 300px;
+    text-overflow: ellipsis;
+  }
+  .conta-name {
+    font-size: 1.2rem;
+  }
 </style>
