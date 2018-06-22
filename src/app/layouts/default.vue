804f6bea4f8f612f40052f165fa15867f4bc2560
Raphael Henrique <raphael.henrique@wtis.com.br>
2018-06-22 17:32:02 -0300

    dashboard menu (prototype)

--- src/app/layouts/default.vue
+++ src/app/layouts/default.vue
@@ -1,6 +1,7 @@
 <script>
 import { openURL } from 'quasar'
 import { mapGetters } from 'vuex'
+import { isObjEmpty } from '../../utils/helpers/utils'
 
 export default {
   name: 'LayoutDefault',
@@ -11,10 +12,13 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(['isLogged', 'currentUser']),
+    ...mapGetters(['isLogged', 'currentUser', 'currentEstabelecimento']),
     userName () {
       let n = this.currentUser.nome.trim().split(' ')
       return n.length > 1 ? n[0] + ' ' + n[n.length - 1] : n[0]
+    },
+    isCurrentEstabelecimentoVazio () {
+      return isObjEmpty(this.currentEstabelecimento)
     }
   },
   methods: {
@@ -34,6 +38,20 @@ export default {
               <li class="bm-dropdown-item">Perfil</li>
                 <router-link to="/logout" tag="li" class="bm-dropdown-item">Sair</router-link>
             </ul>
+          </div>
+        </div>
+      </div>
+      <div class="menu-bar" v-if="!isCurrentEstabelecimentoVazio">
+        <div class="dashboard-menu">
+          <div class="tabs" id="tabs" md-autoselect="">
+            <div class="tab active" ui-sref-active="active" ui-sref="account.dashboard" href="/a/solutionplaceblindagem/dashboard">Dashboard</div>
+            <div class="tab" ui-sref-active="{'active': 'account.schedule.*'}" ui-sref="account.schedule.list" ng-hide="vm.account.isExpired==true" aria-hidden="false" href="/a/solutionplaceblindagem/schedule">Agendar Publicação <span class="bm-badge bm-badge-default">Beta</span>
+            </div>
+            <div class="tab" href="/a/solutionplaceblindagem/profiles">Público-alvo</div>
+            <div class="tab" href="/a/solutionplaceblindagem/dm">Mensagens diretas</div>
+            <div class="tab" href="/a/solutionplaceblindagem/settings">Configurações</div>
+            <div class="tab" href="/a/solutionplaceblindagem/safelist">Listas seguras</div>
+            <div class="tab" href="/a/solutionplaceblindagem/plan">Assinatura</div>
           </div>
         </div>
       </div>
