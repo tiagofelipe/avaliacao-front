8fe5536d7e89ac324cfdc8f388c2f04bf1136c26
Raphael Henrique <raphael.henrique@wtis.com.br>
2018-06-23 11:07:26 -0300

    clear estabelecimento storage when route is '/'

--- src/router/beforeEach.js
+++ src/router/beforeEach.js
@@ -14,6 +14,11 @@ export default (to, from, next) => {
   console.log('não é auth', !isAuthRoute(to), to.path)
   console.log('não está logado', !isLogged())
   console.log('não é rota pública', !includes(publicRoutes, to.path))
+
+  if (to.path === '/') {
+    store.dispatch('clearCurrentEstabelecimento')
+      .then(() => { delete localStorage.estabelecimento })
+  }
   // ----------
   if (!isAuthRoute(to) && !isLogged()) {
     if (auth.loggedIn()) { // O usuário está logado (localStorage cache) mas as informações não estão no store
