98dbbc7263faf9a498e31c883997221287379378
Raphael Henrique <raphael.henrique@wtis.com.br>
2018-06-24 19:51:44 -0300

    adjustments

--- src/app/dashboard/Main.vue
+++ src/app/dashboard/Main.vue
@@ -15,10 +15,10 @@ export default {
     </div>
     <div class="col-md-4 col-xs-12 col-sm-12">
       <div class="row">
-        <info-box info="Avaliações" value="1234" class="col-xs-12 col-sm-6 col-md-12"></info-box>
-        <info-box info="resenhas" value="55" class="col-xs-12 col-sm-6 col-md-12"></info-box>
-        <info-box info="Funcionários cadastrados" value="79" class="col-xs-12 col-sm-6 col-md-12"></info-box>
-        <info-box info="TESTE" value="939399" class="col-xs-12 col-sm-6 col-md-12"></info-box>
+        <info-box info="Avaliações" :value="1234" class="col-xs-12 col-sm-6 col-md-12"></info-box>
+        <info-box info="resenhas" :value="55" class="col-xs-12 col-sm-6 col-md-12"></info-box>
+        <info-box info="Funcionários cadastrados" :value="79" class="col-xs-12 col-sm-6 col-md-12"></info-box>
+        <info-box info="TESTE" :value="939399" class="col-xs-12 col-sm-6 col-md-12"></info-box>
       </div>
     </div>
   </div>
