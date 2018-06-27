ffe186eadd3527f592c3481a2c314426e866e569
Raphael Henrique <raphael.henrique@wtis.com.br>
2018-06-27 18:06:56 -0300

    layout adjustments: added rating on estabelecimento list

--- src/app/main/components/EstabelecimentoItem.vue
+++ src/app/main/components/EstabelecimentoItem.vue
@@ -7,6 +7,26 @@ export default {
       required: true
     }
   },
+  beforeMount () {
+    this.estabelecimento.nota = 4.2
+  },
+  filters: {
+    formatarNota (value) { // TODO: OTIMIZAR ISSO
+      if (typeof value === 'undefined') {
+        return
+      }
+
+      if (value < 1) {
+        return '(sem avaliações)'
+      }
+      value = value.toString()
+
+      if (/^\d{1,}$/.test(value)) {
+        return value + ',0'
+      }
+      return value.replace(/\./, ',')
+    }
+  },
   methods: {
     selecionaEstabelecimento () {
       this.$store.dispatch('setCurrentEstabelecimento', {...this.estabelecimento})
@@ -19,38 +39,40 @@ export default {
 </script>
 
 <template>
-    <div class="conta panel-active">
-      <div class="conta-head">
-        <span class="conta-name"><i class="fa fa-instagram"></i> {{ estabelecimento.nomeFantasia }}</span>
-      </div>
-      <div class="heading-conta">
-        <img class="img-circle profile-pic" src="https://api.bume.io/api/accounts/solutionplaceblindagem/picture">
-        <div class="seguidores">
-          <strong class="ng-binding">12.229</strong> <span>Seguidores</span>
-        </div>
-        <!-- Exibir este bloco abaixo apenas se houver novos seguidores -->
-        <div class="seguidores">
-          <strong class="ng-binding">
-            <i ng-show="a.newFollowersSinceStart" class="fa fa-caret-up" aria-hidden="false">
-
-            </i>2.364</strong> <span>Novos</span>
-        </div>
-      </div>
-      <div class="infos-conta">
-        <!-- Conta ativa sem erros -->
-        <div class="plan-content">
-          <div class="plan-data">
-            <!--p ng-if="a.planTitle &amp;&amp; !a.remainingTime" class="ativa ng-scope">Conta Ativa</p>
-            <p ng-if="a.planTitle &amp;&amp; !a.remainingTime" class="ng-binding ng-scope">(Plano Mensal)</p-->
-          </div>
-          <a class="">
-            <button class="bm-button bm-button-small bm-button-secondary" @click="selecionaEstabelecimento()">Dashboard</button>
-          </a>
-        </div>
+  <div class="conta panel-active">
+    <div class="conta-head">
+      <div class="estabelecimento-rating">
+        <q-rating readonly slot="subtitle" v-model="estabelecimento.nota" :max="5" v-if="estabelecimento.nota > 0"></q-rating>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span class="text-faded" :class="{ 'small-text': estabelecimento.nota < 1 }">{{ estabelecimento.nota | formatarNota }}</span>
       </div>
     </div>
+    <div class="heading-conta">
+      <img class="img-circle profile-pic" src="https://api.bume.io/api/accounts/solutionplaceblindagem/picture">
+      <strong class="conta-name">{{ estabelecimento.nomeFantasia }}</strong>
+    </div>
+    <div class="infos-conta">
+      <div class="plan-content">
+        <div class="plan-data">
+        </div>
+        <a class="">
+          <button class="bm-button bm-button-small bm-button-secondary" @click="selecionaEstabelecimento()">Dashboard</button>
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+  .estabelecimento-rating {
+    font-size: 1.5rem;
+    display: flex;
+    justify-content: center;
+  }
+  .heading-conta>img {
+    margin-right: 10px !important;
+    flex: 1;
+  }
+  .heading-conta>strong {
+    flex: 2;
+  }
 </style>
